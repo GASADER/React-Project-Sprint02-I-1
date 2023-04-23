8291a884c773c2b0bf7bdfbe37dec871126c06cf
GASADER <kajorhkaen@gmail.com>
2023-04-23 13:39:39 +0700

    Edit: nav style

--- gymmini/src/components/navber.js
+++ gymmini/src/components/navber.js
@@ -1,23 +1,23 @@
-export default function Navber(){
-    return(
-        <nav className="flex justify-between p-4">
-        <div className="logo-container">
-            <a href="/">
-                <img src="/" alt="logo" />
-            </a>
-        </div>
-        <div className="nav-account-container flex flex-row">
+export default function Navber() {
+  return (
+    <nav className="nav">
+      <div className="logo-container">
+        <a href="/">
+          <img src="/" alt="logo" id="logoimg" />
+        </a>
+      </div>
+      <div className="nav-account-container">
         <p className="pr-2">Have an account?</p>
-            <ul className="flex flex-row gap-1">
-                <li className="text-red-500">
-                    <a href="/">Login</a>
-                </li>
-                <p>or</p>
-                <li className="text-red-500">
-                    <a href="/">Register</a>
-                </li>
-            </ul>
-        </div>
-        </nav>
-    )
+        <ul className="nav-link">
+          <li className="text-red-500">
+            <a href="/">Login</a>
+          </li>
+          <p>or</p>
+          <li className="text-red-500">
+            <a href="/">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
