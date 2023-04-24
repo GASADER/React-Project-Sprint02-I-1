6ee26289ad8e378f8a9ac440f2c94fa263eaa1ad
GASADER <kajorhkaen@gmail.com>
2023-04-24 09:55:16 +0700

    Add: fortawesome, card, debug border

--- gymmini/src/pages/index.js
+++ gymmini/src/pages/index.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import Navber from '@/components/navber.js'
+import Card from '@/components/card'
 
 export default function Home() {
   return (
     <>
-    <Navber/>
-    <div>
-      hi
-    </div>
+    <Navber />
+    <Card />
     </>
   )
 }
