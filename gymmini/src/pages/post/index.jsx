3aae5b9b6e655ff9fbbf5ee8eadd0bdd77f1ad4f
GASADER <kajorhkaen@gmail.com>
2023-05-16 22:05:15 +0700

    Add:Post page

--- gymmini/src/pages/post/index.jsx
+++ gymmini/src/pages/post/index.jsx
@@ -1,15 +1,189 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@/components/layout";
-import Mockserver from "@/components/mock";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-export default function Home() {
+const postSchema = Yup.object().shape({
+  imageUrl: Yup.string(),
+  type: Yup.string().oneOf([
+    "Biking",
+    "Walking",
+    "Swimming",
+    "Hiking",
+    "Running",
+  ]),
+  distance: Yup.number().required(),
+  duration: Yup.object().shape({
+    hr: Yup.number().required("Hours is required"),
+    min: Yup.number().required("Minutes is required"),
+  }),
+  title: Yup.string().required(),
+  description: Yup.string().required(),
+});
+
+
+function readFileAsBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+      setImagePreview(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+export default function PostActivity() {
+const [imagePreview, setImagePreview] = useState("")
+
+  const handleSubmit = async (values) => {
+    try {
+      console.log("asasa");
+      const file = values.imageUrl;
+      if (file) {
+        const base64 = await readFileAsBase64(file);
+        values.imageUrl = base64;
+      }
+      console.log(values);
+      const response = await axios.get("http://127.0.0.1:3001", values);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Layout>
-        555555
-      <Mockserver />
-      </Layout>
+        <div className=" flex flex-col items-center">
+          <Formik
+            initialValues={{
+              imageUrl: "",
+              type: "",
+              distance: "",
+              duration: { hr: "", min: "" },
+              date: "",
+              title: "",
+              description: "",
+            }}
+            validationSchema={postSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, touched, setFieldValue }) => (
+              <Form className="flex flex-col gap-2">
+                <div className="container flex flex-col  gap-4 ">
+                  <Field name="imageUrl">
+                    {({ field }) => (
+                      <div>
+                        <input
+                          type="file"
+                          className="text-white"
+                          onChange={(event) => {
+                            setFieldValue(
+                              "imageUrl",
+                              event.currentTarget.files[0]
+                            );
+                            setImagePreview(
+                              URL.createObjectURL(event.currentTarget.files[0])
+                            );
+                          }}
+                        />
+                        {imagePreview && (
+                          <img
+                            src={imagePreview}
+                            alt="Image Preview"
+                            width="200"
+                          />
+                        )}
+                        {errors.imageUrl && touched.imageUrl && (
+                          <div>{errors.imageUrl}</div>
+                        )}
+                      </div>
+                    )}
+                  </Field>
 
+                  <div className="container flex gap-4 ">
+                    <label htmlFor="type" className="text-white flex">
+                      Type
+                    </label>
+                    {errors.type && touched.type ? (
+                      <div>{errors.type}</div>
+                    ) : null}
+                  </div>
+                  <Field className="text-black" name="type" as="select">
+                    <option value="">Select a type</option>
+                    <option value="Biking">Biking</option>
+                    <option value="Walking">Walking</option>
+                    <option value="Swimming">Swimming</option>
+                    <option value="Hiking">Hiking</option>
+                    <option value="Running">Running</option>
+                  </Field>
+
+                  <div className="container flex gap-4 ">
+                    <label htmlFor="distance" className="text-white">
+                      Distance
+                    </label>
+                    {errors.distance && touched.distance ? (
+                      <div>{errors.distance}</div>
+                    ) : null}
+                  </div>
+                  <Field name="distance" type="number" />
+
+                  <label htmlFor="duration" className="text-white">
+                    Duration
+                  </label>
+                  <div>
+                    <div className="flex gap-4">
+                      <label htmlFor="duration.hr" className="text-white">
+                        hr
+                      </label>
+                      <Field name="duration.hr" type="text" />
+                    </div>
+                    {errors.duration &&
+                      touched.duration &&
+                      errors.duration.hr && <div>{errors.duration.hr}</div>}
+                  </div>
+
+                  <div>
+                    <div className="flex gap-4">
+                      <label htmlFor="duration.min" className="text-white">
+                        min
+                      </label>
+                      <Field name="duration.min" type="text" />
+                    </div>
+                    {errors.duration &&
+                      touched.duration &&
+                      errors.duration.min && <div>{errors.duration.min}</div>}
+                  </div>
+
+                  <div className="container flex gap-4 ">
+                    <label htmlFor="title" className="text-white">
+                      Title
+                    </label>
+                    {errors.title && touched.title ? (
+                      <div>{errors.title}</div>
+                    ) : null}
+                  </div>
+                  <Field name="title" />
+
+                  <div className="container flex gap-4 ">
+                    <label htmlFor="description" className="text-white">
+                      Description
+                    </label>
+                    {errors.description && touched.description ? (
+                      <div>{errors.description}</div>
+                    ) : null}
+                  </div>
+                  <Field name="description" type="description" />
+                  <button type="submit">Submit</button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </Layout>
     </>
   );
 }
