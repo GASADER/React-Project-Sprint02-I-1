1a16ff4f1cdcc7cc0a7d53989804fc003b92585e
GASADER <kajorhkaen@gmail.com>
2023-05-17 11:02:26 +0700

    Feat: connect with DB

--- server/index.js
+++ server/index.js
@@ -1,35 +1,33 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config.js'
+import "dotenv/config.js";
 import routers from "./routers/router.js";
 import bodyParser from "body-parser";
-
-
+import connectDb from "./config/MongoDB.js";
 
 const ipAddress = process.env.API_IPADDRESS;
 const port = process.env.API_PORT;
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-  };
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+async function run() {
 
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
+  await connectDb()
 
+  routers(app);
 
-async function run(){
-
-    routers(app)
-    await app.listen(port,ipAddress,()=>{
-        console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
-        console.log(`Address: http://${ipAddress}:${port}`);
-      })
+  await app.listen(port, ipAddress, () => {
+    console.log(
+      `Web Application Server is running on ${ipAddress} port ${port}`
+    );
+    console.log(`Address: http://${ipAddress}:${port}`);
+  });
 }
-run()
-
-
-
+run();
