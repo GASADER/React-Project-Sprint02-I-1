f52ac18c4a1cd05647f406b2acde7fb309f8a72a
GASADER <kajorhkaen@gmail.com>
2023-05-23 10:38:57 +0700

    Fix:navber

--- gymmini/src/components/navber.jsx
+++ gymmini/src/components/navber.jsx
@@ -42,13 +42,15 @@ export default function Navber() {
       </div>
       {isLoggedIn ? (
         <div className="nav-account-container gap-4">
+        <div className="w-full h-auto items-end justify-center flex flex-col">
           <p>Welcome {userName}</p>
+          <a className="text-red-500 cursor-pointer text-sm" onClick={logout}>
+            Logout&nbsp;?
+          </a>
+        </div>
           <Link className="w-8 h-auto border-yellow-500 border rounded-full aspect-square" href="/profile">
           <Image className="w-full rounded-full" src={userImage}/>
           </Link>
-          <a className="text-red-500 cursor-pointer" onClick={logout}>
-            Logout&nbsp;?
-          </a>
         </div>
       ) : (
         <div className="nav-account-container">
