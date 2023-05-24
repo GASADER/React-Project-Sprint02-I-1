4efc79dc8b0da79edfa187b90727f37660935370
GASADER <kajorhkaen@gmail.com>
2023-05-24 22:24:03 +0700

    Fix: nav

--- gymmini/src/components/navber.jsx
+++ gymmini/src/components/navber.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import img from '../../public/user.png'
+import img from "../../public/user.png";
 import Image from "next/image";
-
-function logout() {
-  const keys = Object.keys(localStorage);
-  keys.forEach((key) => {
-    localStorage.removeItem(key);
-  });
-  window.location.reload();
-}
+import { useRouter } from "next/router";
 
 
 export default function Navber() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userName, setUsername] = useState('');
-  const [userImage, setUserimage] = useState('');
+  const [userName, setUsername] = useState("");
+  const [userImage, setUserimage] = useState("");
+  const router = useRouter();
   
+  function logout() {
+    const keys = Object.keys(localStorage);
+    keys.forEach((key) => {
+      localStorage.removeItem(key);
+    });
+    router.push(`/`);
+    window.location.reload();
+  }
+
   useEffect(() => {
-    const storedUsername  = localStorage.getItem("username")
-    const storedUserImage  = localStorage.getItem("userImage")
+    const storedUsername = localStorage.getItem("username");
+    const storedUserImage = localStorage.getItem("userImage");
     const userId = localStorage.getItem("userId");
     setIsLoggedIn(!!userId);
-    if (storedUsername&&storedUsername!== "undefined") {
+    if (storedUsername && storedUsername !== "undefined") {
       setUsername(storedUsername);
     } else {
       setUsername("User");
     }
-    if (storedUserImage&&storedUserImage!== "undefined") {
+    if (storedUserImage && storedUserImage !== "undefined") {
       setUserimage(storedUserImage);
     } else {
       setUserimage(img);
@@ -42,14 +45,23 @@ export default function Navber() {
       </div>
       {isLoggedIn ? (
         <div className="nav-account-container gap-4">
-        <div className="w-full h-auto items-end justify-center flex flex-col">
-          <p>Welcome {userName}</p>
-          <a className="text-red-500 cursor-pointer text-sm" onClick={logout}>
-            Logout&nbsp;?
-          </a>
-        </div>
-          <Link className="w-8 h-auto border-yellow-500 border rounded-full aspect-square" href="/profile">
-          <Image className="w-full rounded-full" src={userImage}/>
+          <div className="w-full h-auto items-end justify-center flex flex-col">
+            <p>Welcome {userName}</p>
+            <a className="text-red-500 cursor-pointer text-sm" onClick={logout}>
+              Logout&nbsp;?
+            </a>
+          </div>
+          <Link
+            className="w-8 h-auto border-yellow-500 border rounded-full aspect-square"
+            href="/profile"
+          >
+              <Image
+                className="w-full h-full rounded-full"
+                src={userImage}
+                alt="User Image"
+                width={8}
+                height={8}
+              />
           </Link>
         </div>
       ) : (
