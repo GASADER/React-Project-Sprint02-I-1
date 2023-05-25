5e478201c8527497e6700f65f67e31d22063315a
GASADER <kajorhkaen@gmail.com>
2023-05-26 00:22:02 +0700

    Fix: fill data profile

--- gymmini/src/pages/profile/index.jsx
+++ gymmini/src/pages/profile/index.jsx
@@ -19,6 +19,12 @@ export default function profile() {
     router.push(`/profile/edit/`);
   }
 
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
 
@@ -37,7 +43,7 @@ export default function profile() {
 
       try {
         setIsLoading(true)
-        const response = await axiosInstance.get(`/api/users/${userId}/posts`);
+        const response = await axiosInstance.get(`/api/users/${userId}`);
         console.log(response.data);
         setResponseData(response.data);
         setIsLoading(false);
@@ -61,6 +67,12 @@ export default function profile() {
     id: Id,
     username: username,
     userImage: userImage,
+    firstName: responseData ? responseData.firstName : "",
+  lastName: responseData ? responseData.lastName : "",
+  birthDate: responseData ? formatDate(responseData.birthDate) : "",
+  gender: responseData ? responseData.gender : "",
+  height: responseData ? responseData.height : "",
+  weight: responseData ? responseData.weight : ""
   };
 
   const data = responseData ? [responseData] : [];
@@ -77,37 +89,40 @@ export default function profile() {
             <div className="profileHead border rounded-lg flex m-8">
               <div className="profileimg grow flex flex-col items-center py-4">
                 <div className="profileimgtitle flex-col mb-4"> {item.username}</div>
-                <div className="profileimgcontainer w-36 h-36 rounded-full border border-yellow-500">
+                <div className="profileimgcontainer w-36 h-36 rounded-full border border-yellow-500 m-4">
                   <Image
                     className="profile-img w-full rounded-full aspect-square "
                     src={userImage}
                   />
                 </div>
               </div>
-              <div className="profileinfo flex-col p-4">
-                <div className="rank w-full">Rank Beginner</div>
-                <div className="infodata grid grid-cols-4 grid-rows-2 gap-4 auto-cols-max auto-rows-max py-2">
+              <div className="profileinfo flex-col p-4 ">
+              <div className="flex w-1/2">
+                <div className="rank w-full text-yellow-400">Rank </div>
+                <div className="rankinfo w-full">Beginner</div>
+              </div>
+                <div className="infodata grid grid-cols-4 grid-rows-2 auto-cols-max auto-rows-max py-2 items-center ">
                   <label htmlFor="firstName">FirstName</label>
-                  <div className="firstName h-full">
+                  <div className="firstName ">
                     {item.firstName}
                   </div>
 
                   <label htmlFor="lastName">LastName</label>
-                  <div className="lastNameme h-full">
+                  <div className="lastNameme ">
                     {item.lastName}
                   </div>
 
                   <label htmlFor="birthDate">BirthDate</label>
-                  <div className="birthDate  h-full">{item.birthDate}</div>
+                  <div className="birthDate  ">{item.birthDate}</div>
 
                   <label htmlFor="gender">Gender</label>
-                  <div className="gender  h-full">{item.gender}</div>
+                  <div className="gender  ">{item.gender}</div>
 
                   <label htmlFor="height">Height</label>
-                  <div className="height  h-full">{item.height}</div>
+                  <div className="height ">{item.height}</div>
 
                   <label htmlFor="weight">Weight</label>
-                  <div className="weight h-full">{item.weight}</div>
+                  <div className="weight ">{item.weight}</div>
                 </div>
               </div>
               <div className="profilesetting grow  flex items-start py-8">
@@ -117,7 +132,7 @@ export default function profile() {
                 </button>
               </div>
             </div>
-            <div className="w-full border border-white my-4"></div>
+            <div className="w-full border border-white mx-4"></div>
             <div className="yourPost px-8 py-4 text-2xl">YourPost</div>
             <Card prop={card} />
           </div>
